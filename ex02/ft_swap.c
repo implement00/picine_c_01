af8d6d1e025151d977a654d01878e1f044b57c9e
tripped <tripped@tripped.home>
2025-06-12 01:20:57 +0100

    Final changes, can easily be reviewed rn

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -11,15 +11,11 @@ void ft_swap(int *a, int *b) {
 int main(void) {
     int a;
     int b; 
-    int *ptr_a;
-    int *ptr_b;
 
     a = 10;
     b = 5;
-    ptr_a = &a;
-    ptr_b = &b;
     printf("Current value a; %d Current value b: %d \n", a, b);
-    ft_swap(ptr_a, ptr_b);
+    ft_swap(&a, &b);
     printf("After swap value a; %d Current value b: %d \n", a, b);
     return 0;
 }
